1a4ae04247a941d6cb555df79e727e7109d46cd9
andriusmw <andriusmw@yahoo.es>
2024-02-20 17:00:57 +0100

    receiving all superheros from sql database

--- SuperHerosNet/Controllers/SuperHeroController.cs
+++ SuperHerosNet/Controllers/SuperHeroController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components.Web.Virtualization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHerosNet.Controllers.Data;
 using SuperHerosNet.Controllers.Entities;
 
 namespace SuperHerosNet.Controllers
@@ -9,20 +11,18 @@ namespace SuperHerosNet.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
         {
-            var heroes = new List<SuperHero>
-            {
-                new SuperHero
-                {
-                    Id = 1,
-                    Name = "Spiderman",
-                    FirstName = "Peter",
-                    LastName = "Parker",
-                    Place = "New York City"
-                }
-            };
+            var heroes = await _context.SuperHeroes.ToListAsync();
+         
             return Ok(heroes);
         }
     }
