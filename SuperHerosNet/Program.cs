d002ec8ecc963d8b74838af9e89ba634fe3516aa
andriusmw <andriusmw@yahoo.es>
2024-02-20 16:16:46 +0100

    Update-Database working

--- SuperHerosNet/Program.cs
+++ SuperHerosNet/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DataContext>(options =>
     {
-        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnetion"));
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
     }
 );
 
