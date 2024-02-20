17e85827c5859bea1bb9251a68021d9f96df6486
andriusmw <andriusmw@yahoo.es>
2024-02-20 09:41:26 +0100

    Agregar archivos de proyecto.

---
+++ SuperHerosNet/Controllers/Entities/SuperHero.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperHerosNet.Controllers.Entities
+{
+    public class SuperHero
+    {
+
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+
+    }
+}
---
+++ SuperHerosNet/Controllers/SuperHeroController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHerosNet.Controllers.Entities;
+
+namespace SuperHerosNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllHeroes()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Spiderman",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+            return Ok(heroes);
+        }
+    }
+
+
+}
