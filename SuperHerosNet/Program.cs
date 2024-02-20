cd10d8d4d797418d482bfc0caf2f22cfc9175a8a
andriusmw <andriusmw@yahoo.es>
2024-02-20 15:52:41 +0100

    add migration 30 min

--- SuperHerosNet/Program.cs
+++ SuperHerosNet/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SuperHerosNet.Controllers.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnetion"));
+    }
+);
 
 var app = builder.Build();
 
